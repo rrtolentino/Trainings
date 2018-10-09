183dd097aa0cb109b607942b6666bb80de26b215
Roel Tolentino <roel@opswerks.com>
2018-10-08 17:36:13 -0700

    update on script sec9-ex3

--- bash-scripting/sec9-ex3.sh
+++ bash-scripting/sec9-ex3.sh
@@ -3,6 +3,12 @@
 # Write a shell script that that allows a user to select an action from the menu. The actions are to show the disk usage, show the uptime on the system, and show the users that are logged into the system. Tell the user to enter q to quit. Display "Goodbye!" just before the script exits.
 # If the user enters anything other than 1,2,3,or q,tell them that it is an "Invalid option."
 
+command_execute() {
+   $1
+   read -p "Press any key to continue" ANYKEY
+   echo ""
+}
+
 while true
 do
    clear
@@ -13,19 +19,13 @@ do
   
    case $USERINPUT in
       1)
-         df -h
-         read -p "Press any key to continue" ANYKEY
-         echo ""
+         command_execute df
          ;;
       2)
-         uptime
-         read -p "Press any key to continue" ANYKEY
-         echo ""
+         command_execute uptime
          ;;
       3)
-         who
-         read -p "Press any key to continue" ANYKEY
-         echo ""
+         command_execute who
          ;;
       q|Q)
          echo "Goodbye!"
